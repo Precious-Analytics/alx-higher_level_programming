9c5cc1a735f26f8423daa3b8e1249f281a9f0c43
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2024-01-06 04:54:40 -0800

    Updated 100-print_tebahpla.py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
     case = 'lower' if char % 2 != 0 else 'upper'
-    print("{}".format(chr(char).upper() if case == 'lower' else chr(char)), end="")
+    print(
+            "{}".format(chr(char).upper() if case == 'lower' else chr(char)), 
+            end=""
+            )
