16ea99af6b4d6a4de0ceae310afe8a4c03274dcf
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2024-01-28 07:23:39 -0800

    Created 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+
+"""Define the Square class."""
+
+class Square:
+    """Attributes of the class."""
+
+    def __init__(self, size):
+        """Instances of the object."""
+        self.__size = size
