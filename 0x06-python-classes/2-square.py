ec81bed9db89427ed5b75f10f61ab7497b82e14f
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2024-01-28 07:46:15 -0800

    Created 2-square.py file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/bin/usr/python3
+class Square:
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square (default is 0).
+                         Must be a non-negative integer.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        # Use double underscore to make 'size' a private attribute
+        self.__size = size
+
+        # Check if 'size' is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        # Check if 'size' is non-negative
+        elif size < 0:
+            raise ValueError("size must be >= 0")
